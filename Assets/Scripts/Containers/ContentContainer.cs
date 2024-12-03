6f6430a62702a7dee3215ba3a73a8228a8b1526a
Tallon <srmarcovich@gmail.com>
2024-12-02 21:32:22 -0300

    Creating resources add buttons and deactivating some header buttons

--- Assets/Scripts/Containers/ContentContainer.cs
+++ Assets/Scripts/Containers/ContentContainer.cs
@@ -6,102 +6,104 @@ using ScriptableObjects.Data;
 using UIToolkit.CustomControls;
 using UIToolkit.CustomControls.DataButtons;
 using UnityEngine.UIElements;
-using UnityEngine.WSA;
 
-public class ContentContainer
+namespace Containers
 {
-    private ItemDatabase _database;
-
-    public VisualElement Container { get; private set; }
-    public VisualElement FilterContainer { get; private set; }
-    public VisualElement CardsContainer { get; private set; }
-
-    private Label _title;
-
-    //Current Data
-    private List<Card> _cards = new List<Card>();
-
-    private ContentType _currentContentType;
-    private RarityData _currentRarity;
-
-    public ContentContainer(VisualElement container, ItemDatabase itemDatabase)
+    public class ContentContainer
     {
-        _database = itemDatabase;
+        private ItemDatabase _database;
 
-        Container = container;
-        FilterContainer = Container.Q("content-filter-container");
-        CardsContainer = Container.Q("content-scroll-view").Q("unity-content-container");
-        
-        _title = Container.Q<Label>("content-title");
+        public VisualElement Container { get; private set; }
+        public VisualElement FilterContainer { get; private set; }
+        public VisualElement CardsContainer { get; private set; }
 
-        ConfigFiltersBehaviour();
-    }
+        private Label _title;
 
-    public void ConfigFiltersBehaviour()
-    {
-        var filters = FilterContainer.Query<FilterButton>().ToList();
-        foreach (var filter in filters)
+        //Current Data
+        private List<Card> _cards = new List<Card>();
+
+        private ContentType _currentContentType;
+        private RarityData _currentRarity;
+
+        public ContentContainer(VisualElement container, ItemDatabase itemDatabase)
         {
-            filter.OnSelected += _ =>
-            {
-                _currentRarity = filter.Rarity;
-                UpdateContent();
-            };
+            _database = itemDatabase;
 
-            filter.RegisterCallback<ClickEvent>(evt =>
+            Container = container;
+            FilterContainer = Container.Q("content-filter-container");
+            CardsContainer = Container.Q("content-scroll-view").Q("unity-content-container");
+        
+            _title = Container.Q<Label>("content-title");
+
+            ConfigFiltersBehaviour();
+        }
+
+        public void ConfigFiltersBehaviour()
+        {
+            var filters = FilterContainer.Query<FilterButton>().ToList();
+            foreach (var filter in filters)
             {
-                if (!filter.IsEnabled) return;
+                filter.OnSelected += _ =>
+                {
+                    _currentRarity = filter.Rarity;
+                    UpdateContent();
+                };
+
+                filter.RegisterCallback<ClickEvent>(evt =>
+                {
+                    if (!filter.IsEnabled) return;
                 
-                filters.ForEach(_ => _.Deselect());
-                filter.Select();
-            });
+                    filters.ForEach(_ => _.Deselect());
+                    filter.Select();
+                });
+            }
         }
-    }
 
-    public void SetContent(ContentType contentType)
-    {
-        if (_currentContentType == contentType) return;
-
-        _currentContentType = contentType;
-        UpdateContent();
-    }
-
-    private void UpdateContent()
-    {
-        ClearCardContainer();
-
-        _title.text = _currentContentType.Title.ToUpper();
-        
-        var items = _database.GetItemsByType(_currentContentType);
-        if (_currentRarity != null)
-            items = items.Where(_ => _.Rarity == _currentRarity);
-
-        foreach (var item in items)
+        public void SetContent(ContentType contentType)
         {
-            var card = CreateCard(item);
-            _cards.Add(card);
+            if (_currentContentType == contentType) return;
+
+            _currentContentType = contentType;
+            UpdateContent();
         }
-    }
 
-    private Card CreateCard(ItemData itemData)
-    {
-        var card = CardsContainer.CreateChild<Card>($"card_{itemData.name.ToLower()}");
-        card.ItemData = itemData;
-        card.OnItemEquipped += HandleItemEquipped;
+        private void UpdateContent()
+        {
+            ClearCardContainer();
 
-        return card;
-    }
+            _title.text = _currentContentType.Title.ToUpper();
+        
+            var items = _database.GetItemsByType(_currentContentType);
+            if (_currentRarity != null)
+                items = items.Where(_ => _.Rarity == _currentRarity);
 
-    private void ClearCardContainer()
-    {
-        CardsContainer.Clear();
-        _cards.Clear();
-    }
+            foreach (var item in items)
+            {
+                var card = CreateCard(item);
+                _cards.Add(card);
+            }
+        }
 
-    public void HandleItemEquipped(ItemData itemEquipped)
-    {
-        var cardsOfSameType = _cards.Where(_ => _.ItemData != itemEquipped && _.ItemData.ContentType == itemEquipped.ContentType);
-        foreach (var card in cardsOfSameType)
-            card.UnequipItem();
+        private Card CreateCard(ItemData itemData)
+        {
+            var card = CardsContainer.CreateChild<Card>($"card_{itemData.name.ToLower()}");
+            card.ItemData = itemData;
+            card.OnItemEquipped += HandleItemEquipped;
+
+            return card;
+        }
+
+        private void ClearCardContainer()
+        {
+            CardsContainer.Clear();
+            _cards.Clear();
+        }
+
+        public void HandleItemEquipped(ItemData itemEquipped)
+        {
+            var cardsOfSameType = _cards.Where(_ => _.ItemData != itemEquipped && _.ItemData.ContentType == itemEquipped.ContentType);
+            foreach (var card in cardsOfSameType)
+                card.UnequipItem();
+        }
     }
 }
